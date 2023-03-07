d9efde1f03e66c57ccb1a06f3839e1a26fd1b2d6
Moritz Baumgarten <mb2562@cam.ac.uk>
2023-03-07 21:50:32 +0000

    deterministic class and corresponding propagator implemented

---
+++ noqmc/deterministic/__init__.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from noqmc.utils.utilities import Parser
+from noqmc.utils.plot import Plot 
+
+from noqmc.deterministic.system import System
+from noqmc.deterministic.propagator import Propagator
+from noqmc.nomrccmc.postprocessor import Postprocessor
+from noqmc.nomrccmc.statistics import Statistics
+
+from pyscf.gto import Mole
+from pyscf import gto
+from qcmagic.auxiliary.qcmagic_standards import ZERO_TOLERANCE
+
+DEFAULT_DETERMINISTIC_ARGS = {
+        'mode': 'ref',
+        'verbosity': 1,
+        'seed': 69420,
+        'dt': 0.1,
+        'nr_w': 3000,
+        'A': 10,
+        'c': 0.01,
+        'it_nr': 1000,
+        'delay': 200,
+        'theory_level': 2,
+        'benchmark': 1,
+        'localization': 0,
+        'nr_scf': 2,
+}
+
+THRESHOLDS = {
+        'ov_zero_th':       5e-06,
+        'rounding':         int(-np.log10(ZERO_TOLERANCE))-4,
+}
+
+class Deterministic(Propagator):
+        __doc__ = """Executes the deterministic QMC power-method-like propagation.\n""" + Propagator.__doc__
+        def __init__(self, mol: Mole, params = None):
+                if params is not None:
+                        if isinstance(params, dict):
+                                params = params
+                        elif isinstance(params, str):
+                                params = Parser().parse(params)
+                else: params = DEFAULT_DETERMINISTIC_ARGS 
+ 
+                self.params = params
+                self.mol = mol
+                self.system = System(mol=mol, params=params)
+                self.initialized = False
+
+                
+        def run(self) -> Propagator:
+                if not self.initialized: self.initialize_references() 
+                self.prop = Propagator(self.system)
+                self.prop.run()
+                self.__dict__.update(self.prop.__dict__)
+                return self.prop
+
+        def initialize_references(self, guess_rhf: np.ndarray = None, 
+                                  guess_uhf: np.ndarray = None) -> None:
+                r""""""
+                self.system.get_reference(
+                    guess_rhf=guess_rhf, guess_uhf=guess_uhf
+                )
+                self.system.initialize()
+                self.initialized = True
+
+        def get_data(self) -> None:
+                r"""After running the population dynamics, get_data() will be
+                able to extract the Shift, coefficients, projected energy,
+                matrix elements and an error analysis."""
+                self.postpr = Postprocessor(self.prop)
+                self.postpr.postprocessing(benchmark = self.params['benchmark'])
+                                
+                #self.stat = Statistics(self.prop.Ss, self.params)
+                #self.stat.analyse() 
+                #self.postpr.data_summary = self.stat.data_summary
+
+        def plot(self) -> None:
+
+                plot = Plot()
+                data = plot.add_data(self.postpr)
+                plot.setup_figure(data)
+                plot.plot_data()
+
+if __name__ == '__main__':
+        mol = gto.M(atom=[['H', 0, 0, 0], ['H', 0, 0, 1.8]], 
+                    basis='sto-3g', unit='Bohr')
+        
+        my_nociqmc = Deterministic(mol)
+        my_nociqmc.run()
+        my_nociqmc.get_data()
+        my_nociqmc.plot()
+        
---
+++ noqmc/deterministic/propagator.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+#---- author: Moritz Baumgarten ----# 
+#Implementation of a nonorthogonal multireference configuration
+#interaction quantum Monte Carlo (NOMRCI-QMC) method.
+#The Hilbert space for a common NOMRCI-QMC calculation is generated
+#by a subset of possible exitations of the reference determinats
+#(generally obtained from SCF metadynamics)
+#Based on Booth, Thom and Alavi [2009], and Thom and Head-Gordon [2008]
+
+import numpy as np
+import scipy.linalg as la
+import multiprocessing
+import sys, os, shutil
+from typing import (
+    Tuple, 
+    Sequence,
+)    
+
+####QCMAGIC IMPORTS
+from qcmagic.core.cspace.basis.basisset import ConvolvedBasisSet
+from qcmagic.interfaces.converters.pyscf import scf_to_state
+from qcmagic.core.backends.nonorthogonal_backend import (
+    calc_overlap, 
+    calc_hamiltonian, 
+)
+from qcmagic.core.sspace.single_determinant import SingleDeterminant
+
+####CUSTOM IMPORTS
+from noqmc.nomrccmc.system import System
+from noqmc.nomrccmc.propagator import calc_mat_elem
+
+norm = lambda x: x / np.linalg.norm(x, ord = 2) 
+
+def mute():
+    sys.stdout = open(os.devnull, 'w')
+
+class Propagator(System):
+        r"""Class for propagation of the wavefunction/walkers in imaginary time."""
+
+        def __init__(self, system: System) -> None:
+                r"""Inherits parameters from System object.
+
+		:param system: System object containing information about Hilbert space"""
+                self.__dict__.update(system.__dict__)
+                self.E_ref = self.E_HF
+                
+                self.E_proj = np.empty(self.params['it_nr']+1)
+                self.E_proj[0] = self.E_NOCI - self.E_ref        
+
+                self.Ss = np.empty(self.params['it_nr']+1)
+                self.Nws = np.empty(self.params['it_nr'], dtype = int)
+                self.coeffs = np.zeros(
+                    [self.params['it_nr']+1, self.params['dim']]
+                )
+                self.coeffs[0,:] = norm(self.initial.copy())
+                self.S = self.Ss[0] = 0
+                self.curr_it = 0
+                self.n = []
+
+        def calculate_operators(self):
+                r"""Calculates the Hamiltonian and overlap"""        
+                pool = multiprocessing.Pool(
+                    processes = multiprocessing.cpu_count(), 
+                    initializer = mute
+                )
+                processes = {}
+
+                isnan = np.isnan(self.H)
+                indices = np.where(isnan)
+                
+                for i,j in zip(indices[0], indices[1]):
+                        det_i = self.get_det(i)
+                        det_j = self.get_det(j)
+                        occ_i = det_i.occupied_coefficients
+                        occ_j = det_j.occupied_coefficients
+
+                        processes[(i,j)] = pool.apply_async(
+                            calc_mat_elem, 
+                            [occ_i, occ_j, self.cbs, self.enuc, self.sao, self.hcore, self.E_ref]
+                        )
+
+                pool.close()
+                pool.join()
+                for i,j in zip(indices[0], indices[1]):
+                        processes[(i,j)] = processes[(i,j)].get()
+                        self.H[i,j] = processes[(i,j)][0]
+                        self.H[j,i] = processes[(i,j)][1]
+                        self.overlap[i,j] = processes[(i,j)][2]
+                        self.overlap[j,i] = processes[(i,j)][3]
+
+                self.propagator = np.eye(self.params['dim']) - self.params['dt'] * self.H
+
+        def E(self) -> None:
+                r"""Calculates energy estimator at current iteration."""
+                
+                coeffs = self.coeffs[self.curr_it,:]
+                index = np.where(
+                    np.abs(coeffs) == np.max(np.abs(coeffs))
+                )[0][0] #get index of maximum value 
+                E_proj = np.einsum('i,i->', self.H[index,:], coeffs)
+                E_proj /= np.einsum('i,i->', self.overlap[index, :], coeffs)
+                self.E_proj[self.curr_it+1] = E_proj
+                return E_proj
+
+        def population_dynamics(self) -> None:    #parallelize for each scf solution
+                r"""Spawning/Death in one step due to nonorthogonality. 
+		Writes changes to current wavefunction to self.sp_coeffs."""
+
+                E_proj = self.E()
+
+                new = np.dot(self.propagator, self.coeffs[self.curr_it, :])
+                new = norm(new)
+                self.coeffs[self.curr_it+1, :] = new
+
+                print(f'{self.curr_it} E_proj:      ', 
+                    self.E_proj[self.curr_it] 
+                )
+
+        def run(self) -> None:
+                r"""Executes what is essentially the power method.
+                """
+                self.calculate_operators()                
+                for i in range(self.params['it_nr']):
+                        self.curr_it = i
+                        self.Nws[self.curr_it] = sum([int(np.round(np.abs(c))) for c in self.coeffs[self.curr_it, :]])
+                        self.population_dynamics()
+                print(np.linalg.eigh(self.overlap))
+
+if __name__ == '__main__':      
+        pass
+
+
+
