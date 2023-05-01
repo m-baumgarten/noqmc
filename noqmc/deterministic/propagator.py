8087c03ccd3fe21d8df26da70c3bf53cd90fc6a2
m-baumgarten <baumorit@student.ethz.ch>
2023-05-01 20:19:00 +0100

    Cleaned deterinistic propagator and fixed self.propagator such that it converges to the correct NOCI solution even if [H,S]!=0

--- noqmc/deterministic/propagator.py
+++ noqmc/deterministic/propagator.py
@@ -8,30 +8,9 @@
 #Based on Booth, Thom and Alavi [2009], and Thom and Head-Gordon [2008]
 
 import numpy as np
-import multiprocessing
-import sys, os
-from typing import (
-    Tuple, 
-    Sequence,
-)    
-
-####QCMAGIC IMPORTS
-from qcmagic.core.backends.nonorthogonal_backend import (
-    calc_overlap, 
-    calc_hamiltonian, 
-)
-from qcmagic.core.sspace.single_determinant import SingleDeterminant
-
-####CUSTOM IMPORTS
-from noqmc.nomrccmc.system import (
-    System,
-    calc_mat_elem,
-)
+from noqmc.nomrccmc.system import System
 
 norm = lambda x: x / np.linalg.norm(x, ord = 2) 
-
-def mute():
-    sys.stdout = open(os.devnull, 'w')
 
 class Propagator(System):
         r"""Class for propagation of the wavefunction/walkers in imaginary time."""
@@ -41,59 +20,31 @@ class Propagator(System):
 
 		:param system: System object containing information about Hilbert space"""
                 self.__dict__.update(system.__dict__)
-                self.E_ref = self.E_HF
-                
-                self.E_proj = np.empty(self.params.it_nr+1)
-                self.E_proj[0] = self.E_NOCI - self.E_ref        
 
-                self.Ss = np.empty(self.params.it_nr+1)
-                self.Nws = np.empty(self.params.it_nr, dtype = int)
+                self.E_proj = np.empty(self.params.it_nr)
+#                self.Nws = np.empty(self.params.it_nr)
                 self.coeffs = np.zeros(
                     [self.params.it_nr+1, self.params.dim]
                 )
-                self.coeffs[0,:] = norm(self.initial.copy())
+#                self.initial = np.dot(self.overlap, self.initial) #NOTE
+                self.coeffs[0,:] = norm(self.initial.copy()) #* 3000
                 
-                self.coeffs[0,:] = np.random.random(722)
-                self.coeffs[0,:] /= np.linalg.norm(self.coeffs[0,:])
+#                self.coeffs[0,:] = np.random.random(236)
+#                self.coeffs[0,:] /= np.linalg.norm(self.coeffs[0,:])
                 
-                self.S = self.Ss[0] = 0
+
                 self.curr_it = 0
-                self.n = []
 
         def calculate_operators(self):
                 r"""Calculates the Hamiltonian and overlap"""        
-                pool = multiprocessing.Pool(
-                    processes = multiprocessing.cpu_count(), 
-                    initializer = mute
-                )
-                processes = {}
-
-                isnan = np.isnan(self.H)
-                indices = np.where(isnan)
-                
-                for i,j in zip(indices[0], indices[1]):
-                        det_i = self.get_det(i)
-                        det_j = self.get_det(j)
-                        occ_i = det_i.occupied_coefficients
-                        occ_j = det_j.occupied_coefficients
-
-                        processes[(i,j)] = pool.apply_async(
-                            calc_mat_elem, 
-                            [occ_i, occ_j, self.cbs, self.enuc, self.sao, self.hcore, self.E_ref]
-                        )
-
-                pool.close()
-                pool.join()
-                for i,j in zip(indices[0], indices[1]):
-                        processes[(i,j)] = processes[(i,j)].get()
-                        self.H[i,j] = processes[(i,j)][0]
-                        self.H[j,i] = processes[(i,j)][1]
-                        self.overlap[i,j] = processes[(i,j)][2]
-                        self.overlap[j,i] = processes[(i,j)][3]
-
+                self._calculatematrices()
                 self.propagator = np.eye(self.params.dim) - self.params.dt * self.H
+                #self.propagator = self.overlap - self.params.dt * self.H
 
-        def E(self) -> None:
+        def update_propagator(self, E):
+                self.propagator = np.eye(self.params.dim) - self.params.dt * (self.H - E*self.overlap)
+
+        def E(self) -> float:
                 r"""Calculates energy estimator at current iteration."""
                 
                 coeffs = self.coeffs[self.curr_it,:]
@@ -102,7 +53,7 @@ class Propagator(System):
                 )[0][0] #get index of maximum value 
                 E_proj = np.einsum('i,i->', self.H[index,:], coeffs)
                 E_proj /= np.einsum('i,i->', self.overlap[index, :], coeffs)
-                self.E_proj[self.curr_it+1] = E_proj
+                self.E_proj[self.curr_it] = E_proj
                 return E_proj
 
         def population_dynamics(self) -> None:    #parallelize for each scf solution
@@ -110,6 +61,7 @@ class Propagator(System):
 		Writes changes to current wavefunction to self.sp_coeffs."""
 
                 E_proj = self.E()
+                self.update_propagator(E_proj)
 
                 new = np.dot(self.propagator, self.coeffs[self.curr_it, :])
                 new = norm(new)
@@ -125,9 +77,7 @@ class Propagator(System):
                 self.calculate_operators()                
                 for i in range(self.params.it_nr):
                         self.curr_it = i
-                        self.Nws[self.curr_it] = sum([int(np.round(np.abs(c))) for c in self.coeffs[self.curr_it, :]])
                         self.population_dynamics()
-                print(np.linalg.eigh(self.overlap))
 
 if __name__ == '__main__':      
         pass
